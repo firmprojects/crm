08ba2c4998cdece11af57d110846f3ab9535899a
Vihas <vihas007makwana@gmail.com>
2020-07-16 00:36:14 +0530

    Done

---
+++ users/decorators.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import user_passes_test
+
+def admin_check(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.is_superuser,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+def customer_check(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.is_customer,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+def employee_check(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    actual_decorator = user_passes_test(
+        lambda u: u.is_employee and u.is_superuser,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+def client_check(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+    actual_decorator = user_passes_test(
+        lambda u: u.is_client and u.is_superuser,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
