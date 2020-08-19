11d873872e43e806cb2abc4e55619644c3195bfb
firmprojects <66138980+firmprojects@users.noreply.github.com>
2020-08-19 12:54:10 +0100

    updated

--- crm/settings.py
+++ crm/settings.py
@@ -106,11 +106,11 @@ DATABASES = {
         # 'PASSWORD': 'enternow',
         # 'HOST': 'localhost',
         # 'PORT': '5432'
-       ' Host': 'ec2-34-237-89-96.compute-1.amazonaws.com',
-       'Database': 'd3jqqkqp4duur6',
+       'HOST': 'ec2-34-237-89-96.compute-1.amazonaws.com',
+       'NAME': 'd3jqqkqp4duur6',
        'User': 'jdunvxhfsbzydr',
-       'Port':'5432',
-       'Password': 'c90d233314d95372878d00b70a2c73e2ec4ef6f5677822ff42da017a2f9153e4',
+       'PORT':'5432',
+       'PASSWORD': 'c90d233314d95372878d00b70a2c73e2ec4ef6f5677822ff42da017a2f9153e4',
        'URI': 'postgres://jdunvxhfsbzydr:c90d233314d95372878d00b70a2c73e2ec4ef6f5677822ff42da017a2f9153e4@ec2-34-237-89-96.compute-1.amazonaws.com:5432/d3jqqkqp4duur6',
        'Heroku CLI':' heroku pg:psql postgresql-polished-94696 --app firmcrm'
     },
